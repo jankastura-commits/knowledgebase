445cb37c0e05f6533d28917477bd7fa087c3d740
jankastura-commits <jan.kastura@gmail.com>
2025-10-24 14:47:19 +0200

    Create public-config.js

---
+++ netlify/functions/public-config.js
@@ -0,0 +1,11 @@
+// /.netlify/functions/public-config
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json", "Cache-Control": "no-store" },
+    body: JSON.stringify({
+      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      GOOGLE_API_KEY: process.env.GOOGLE_API_KEY
+    })
+  };
+};
